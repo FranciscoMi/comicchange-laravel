4bc9704f5e0cc314b2536af073f5351b19d22773
FranciscoMi <franlope@gmail.com>
2023-05-01 11:40:55 +0200

    errores de frontend 0.1

--- resources/views/layouts/_partials/menu.blade.php
+++ resources/views/layouts/_partials/menu.blade.php
@@ -15,7 +15,7 @@
     @if(Auth::check())
       @include('layouts._partials.btnhome')
       @if(Auth::user()->idrole == 1)
-        <a class="btn btn__transition text--Komika box__stretch btn__close link btn--padding @yield('colorbtnuser')" href="{{route('user.index')}}">Usuarios</a>
+        <a class="btn btn__transition text--Komika box__stretch link btn--padding @yield('colorbtnuser')" href="{{route('user.index')}}">Usuarios</a>
       @endif
         @if (Auth::user()->idrole == 1 || Auth::user()->idrole == 2)
       <a class="btn @yield('colorbtncomics') btn__transition text--Komika box__stretch btn__close btn--padding link" href="{{route('comic.index')}}">Comics</a>
